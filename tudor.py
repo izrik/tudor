21916041710efbc12908a11ac57819a966861ebd
izrik <izrik@izrik.com>
2015-01-27 09:16:27 -0600

    Debug mode.

--- tudor.py
+++ tudor.py
@@ -11,7 +11,8 @@ def hello_world():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true')
 
     args = parser.parse_args()
 
-    app.run()
+    app.run(debug=args.debug)
