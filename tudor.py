17d2c574e947b2c70a0a56906ab50bf1b81d25a7
izrik <izrik@izrik.com>
2015-01-27 10:51:31 -0600

    Create a task class and show a table of tasks.

--- tudor.py
+++ tudor.py
@@ -5,9 +5,22 @@ import argparse
 
 app = Flask(__name__)
 
+task_id = 0
+
+
+class Task(object):
+    def __init__(self, summary):
+        global task_id
+        self.id = task_id
+        task_id += 1
+        self.summary = summary
+
+
+tasks = [Task('do something'), Task('do another thing')]
+
 @app.route('/')
 def index():
-    return render_template('index.t.html')
+    return render_template('index.t.html', tasks=tasks)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
