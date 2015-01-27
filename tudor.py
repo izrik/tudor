6d9e132a8c3b2a04ebd1d2692b444906be59037c
izrik <izrik@izrik.com>
2015-01-27 11:00:18 -0600

    Add new tasks.

--- tudor.py
+++ tudor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for,request
 import argparse
 
 app = Flask(__name__)
@@ -23,6 +23,13 @@ tasks = [Task('do something'), Task('do another thing')]
 @app.route('/')
 def index():
     return render_template('index.t.html', tasks=tasks)
+
+@app.route('/new', methods=['POST'])
+def add_new():
+    summary = request.form['summary']
+    tasks.append(Task(summary))
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
