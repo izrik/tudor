e18bb597447db49d35163a1208881a534bf50c30
izrik <izrik@izrik.com>
2015-09-21 17:20:23 -0500

    Re-arrange test data.

--- test_tudor.py
+++ test_tudor.py
@@ -214,32 +214,33 @@ class DbLoaderDoneDeletedTest(unittest.TestCase):
         db.session.add(child1)
 
         parent2 = Task(summary='parent2')
+        parent2.is_done = True
         child2 = Task(summary='child2')
         child2.parent = parent2
-        grandchild2 = Task(summary='grandchild2')
-        grandchild2.parent = child2
-        great_grandchild2 = Task(summary='great_grandchild2')
-        great_grandchild2.is_done = True
-        great_grandchild2.parent = grandchild2
-        great_great_grandchild2 = Task(summary='great_great_grandchild2')
-        great_great_grandchild2.parent = great_grandchild2
         db.session.add(parent2)
         db.session.add(child2)
-        db.session.add(grandchild2)
-        db.session.add(great_grandchild2)
-        db.session.add(great_great_grandchild2)
 
         parent3 = Task(summary='parent3')
-        parent3.is_done = True
         child3 = Task(summary='child3')
         child3.parent = parent3
+        grandchild3 = Task(summary='grandchild3')
+        grandchild3.parent = child3
+        great_grandchild3 = Task(summary='great_grandchild3')
+        great_grandchild3.is_done = True
+        great_grandchild3.parent = grandchild3
+        great_great_grandchild3 = Task(summary='great_great_grandchild3')
+        great_great_grandchild3.parent = great_grandchild3
         db.session.add(parent3)
         db.session.add(child3)
+        db.session.add(grandchild3)
+        db.session.add(great_grandchild3)
+        db.session.add(great_great_grandchild3)
 
         db.session.commit()
 
-        for t in [normal, done, parent1, child1, parent2, child2, grandchild2,
-                  great_grandchild2, great_great_grandchild2, parent3, child3]:
+        for t in [normal, done, parent1, child1, parent2, child2, parent3,
+                  child3, grandchild3, great_grandchild3,
+                  great_great_grandchild3]:
             self.task_ids[t.summary] = t.id
 
     def test_loader_include_done_no_roots(self):
@@ -263,24 +264,24 @@ class DbLoaderDoneDeletedTest(unittest.TestCase):
         self.assertIsInstance(tasks[1], self.app.Task)
         self.assertIsInstance(tasks[2], self.app.Task)
 
-        expected_summaries = {'normal', 'parent1', 'parent2'}
+        expected_summaries = {'normal', 'parent1', 'parent3'}
         summaries = set(t.summary for t in tasks)
         self.assertEqual(expected_summaries, summaries)
 
     def test_done_children_stop_search(self):
-        tasks = self.app.Task.load(roots=[self.task_ids['parent2']],
+        tasks = self.app.Task.load(roots=[self.task_ids['parent3']],
                                    max_depth=None)
         self.assertEqual(3, len(tasks))
         self.assertIsInstance(tasks[0], self.app.Task)
         self.assertIsInstance(tasks[1], self.app.Task)
         self.assertIsInstance(tasks[2], self.app.Task)
 
-        expected_summaries = {'parent2', 'child2', 'grandchild2'}
+        expected_summaries = {'parent3', 'child3', 'grandchild3'}
         summaries = set(t.summary for t in tasks)
         self.assertEqual(expected_summaries, summaries)
 
     def test_done_children_dont_stop_search_if_included(self):
-        tasks = self.app.Task.load(roots=[self.task_ids['parent2']],
+        tasks = self.app.Task.load(roots=[self.task_ids['parent3']],
                                    max_depth=None,
                                    include_done=True)
         self.assertEqual(5, len(tasks))
@@ -290,8 +291,8 @@ class DbLoaderDoneDeletedTest(unittest.TestCase):
         self.assertIsInstance(tasks[3], self.app.Task)
         self.assertIsInstance(tasks[4], self.app.Task)
 
-        expected_summaries = {'parent2', 'child2', 'grandchild2',
-                              'great_grandchild2', 'great_great_grandchild2'}
+        expected_summaries = {'parent3', 'child3', 'grandchild3',
+                              'great_grandchild3', 'great_great_grandchild3'}
         summaries = set((t.summary for t in tasks))
         self.assertEqual(expected_summaries, summaries)
 
