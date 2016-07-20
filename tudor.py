3dcbd3de0e99eeafa0d6159c3c0ece873595b4eb
izrik <izrik@izrik.com>
2016-07-20 17:06:14 -0500

    Use the ds instead of the app.

--- tudor.py
+++ tudor.py
@@ -126,7 +126,7 @@ def generate_app(db_uri=DEFAULT_TUDOR_DB_URI, ds_factory=None,
     class Options(object):
         @staticmethod
         def get(key, default_value=None):
-            option = app.Option.query.get(key)
+            option = ds.Option.query.get(key)
             if option is None:
                 return default_value
             return option.value
