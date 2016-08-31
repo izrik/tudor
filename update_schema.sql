d78b280d72fc55a240a9f9a577805d807f5a46ca
izrik <izrik@izrik.com>
2016-08-31 11:54:52 -0500

    Manually add an authorized user to all tasks.

--- update_schema.sql
+++ update_schema.sql
@@ -5,3 +5,11 @@ alter table user rename to user2;
 
 insert into user (email, hashed_password, is_admin) select email, hashed_password, is_admin from user2;
 drop table user2;
+
+######
+
+# manually add an authorized user to all tasks
+
+set @user_id = (select id from user where _____ limit 1);
+insert into task_user_link (task_id, user_id) select id as task_id, @user_id from task;
+
