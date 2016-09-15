9889cafd6cada1f58a0fa226d36930af5b423e4b
izrik <izrik@izrik.com>
2016-09-15 11:05:47 -0500

    Test the get_deadlines_data method.

--- run_tests.py
+++ run_tests.py
@@ -15,6 +15,7 @@ from tests.is_user_authorized_or_admin_tests import *
 from tests.task_hierarchy_tests import *
 from tests.sort_by_hierarchy_tests import *
 from tests.get_index_data_tests import *
+from tests.get_deadlines_data_tests import *
 
 
 def run():
