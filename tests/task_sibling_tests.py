a8472e8f04a7bb4b8d44f7f3eb4be790e25ddc85
izrik <izrik@izrik.com>
2017-04-21 16:57:08 -0500

    Test siblings of top-level (i.e. no parent) tasks.

--- tests/task_sibling_tests.py
+++ tests/task_sibling_tests.py
@@ -151,3 +151,116 @@ class TaskSiblingTest(unittest.TestCase):
         self.assertEqual(2, len(result2))
         self.assertIs(t3, result2[0])
         self.assertIs(t2, result2[1])
+
+    def test_gets_toplevel_tasks_with_same_parent(self):
+        # given
+        t2 = self.Task('t2')
+        t2.order_num = 2
+        t3 = self.Task('t3')
+        t3.order_num = 3
+
+        self.db.session.add(t2)
+        self.db.session.add(t3)
+        self.db.session.commit()
+
+        # when
+        result = t2.get_siblings()
+
+        # then
+        self.assertIsNotNone(result)
+        result2 = list(result)
+        self.assertEqual(2, len(result2))
+        self.assertIs(t2, result2[0])
+        self.assertIs(t3, result2[1])
+
+    def test_gets_toplevel_done_tasks(self):
+        # given
+        t2 = self.Task('t2')
+        t2.order_num = 2
+        t3 = self.Task('t3')
+        t3.is_done = True
+        t3.order_num = 3
+
+        self.db.session.add(t2)
+        self.db.session.add(t3)
+        self.db.session.commit()
+
+        # when
+        result = t2.get_siblings()
+
+        # then
+        self.assertIsNotNone(result)
+        result2 = list(result)
+        self.assertEqual(2, len(result2))
+        self.assertIs(t2, result2[0])
+        self.assertIs(t3, result2[1])
+
+    def test_includes_toplevel_deleted_by_default(self):
+        # given
+        t2 = self.Task('t2')
+        t3 = self.Task('t3')
+        t3.is_deleted = True
+
+        self.db.session.add(t2)
+        self.db.session.add(t3)
+        self.db.session.commit()
+
+        # when
+        result = t2.get_siblings()
+
+        # then
+        self.assertIsNotNone(result)
+        result2 = list(result)
+        self.assertEqual(2, len(result2))
+        self.assertIs(t2, result2[0])
+        self.assertIs(t3, result2[1])
+
+    def test_ignores_toplevel_deleted_tasks_when_indicated(self):
+        # given
+        t2 = self.Task('t2')
+        t3 = self.Task('t3')
+        t3.is_deleted = True
+
+        self.db.session.add(t2)
+        self.db.session.add(t3)
+        self.db.session.commit()
+
+        # when
+        result = t2.get_siblings(include_deleted=False)
+
+        # then
+        self.assertIsNotNone(result)
+        result2 = list(result)
+        self.assertEqual(1, len(result2))
+        self.assertIs(t2, result2[0])
+
+    def test_ordered_yields_toplevel_siblings_sorted_by_order_num(self):
+        # given
+        t2 = self.Task('t2')
+        t2.order_num = 2
+        t3 = self.Task('t3')
+        t3.order_num = 3
+
+        self.db.session.add(t2)
+        self.db.session.add(t3)
+        self.db.session.commit()
+
+        # when
+        result = t2.get_siblings(ordered=False)
+
+        # then
+        self.assertIsNotNone(result)
+        result2 = list(result)
+        self.assertEqual(2, len(result2))
+        self.assertIs(t2, result2[0])
+        self.assertIs(t3, result2[1])
+
+        # when
+        result = t2.get_siblings(ordered=True)
+
+        # then
+        self.assertIsNotNone(result)
+        result2 = list(result)
+        self.assertEqual(2, len(result2))
+        self.assertIs(t3, result2[0])
+        self.assertIs(t2, result2[1])
