b05b88e02d9d2cbb3a72c807e081010afb3729c1
izrik <izrik@izrik.com>
2017-04-21 16:54:26 -0500

    Test the 'ordered' parameter.

--- tests/task_sibling_tests.py
+++ tests/task_sibling_tests.py
@@ -116,3 +116,38 @@ class TaskSiblingTest(unittest.TestCase):
         result2 = list(result)
         self.assertEqual(1, len(result2))
         self.assertIs(t2, result2[0])
+
+    def test_ordered_yields_siblings_sorted_by_order_num(self):
+        # given
+        t1 = self.Task('t1')
+        t2 = self.Task('t2')
+        t2.parent = t1
+        t2.order_num = 2
+        t3 = self.Task('t3')
+        t3.parent = t1
+        t3.order_num = 3
+
+        self.db.session.add(t1)
+        self.db.session.add(t2)
+        self.db.session.add(t3)
+        self.db.session.commit()
+
+        # when
+        result = t2.get_siblings(ordered=False)
+
+        # then
+        self.assertIsNotNone(result)
+        result2 = list(result)
+        self.assertEqual(2, len(result2))
+        self.assertIs(t2, result2[0])
+        self.assertIs(t3, result2[1])
+
+        # when
+        result = t2.get_siblings(ordered=True)
+
+        # then
+        self.assertIsNotNone(result)
+        result2 = list(result)
+        self.assertEqual(2, len(result2))
+        self.assertIs(t3, result2[0])
+        self.assertIs(t2, result2[1])
