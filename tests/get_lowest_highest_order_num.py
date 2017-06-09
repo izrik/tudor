964b8970373355b74b46a3e05411efc9172ba3fd
izrik <izrik@izrik.com>
2017-06-09 15:41:33 -0500

    Use slightly different order numbers, so we're sure it's not accidentally returning the _count_ of tasks.

--- tests/get_lowest_highest_order_num.py
+++ tests/get_lowest_highest_order_num.py
@@ -40,35 +40,35 @@ class GetLowestHighestOrderNumTest(unittest.TestCase):
     def test_single_task_lowest_returns_order_num(self):
         # given
         task = self.Task('task')
-        task.order_num = 1
+        task.order_num = 10
         self.pl.add(task)
 
         # when
         order_num = self.ll.get_lowest_order_num()
 
         # then
-        self.assertEqual(1, order_num)
+        self.assertEqual(10, order_num)
 
     def test_single_task_highest_returns_order_num(self):
         # given
         task = self.Task('task')
-        task.order_num = 1
+        task.order_num = 10
         self.pl.add(task)
 
         # when
         order_num = self.ll.get_highest_order_num()
 
         # then
-        self.assertEqual(1, order_num)
+        self.assertEqual(10, order_num)
 
     def test_many_tasks_lowest_returns_lowest(self):
         # given
         t1 = self.Task('t1')
-        t1.order_num = 1
+        t1.order_num = 10
         t2 = self.Task('t2')
-        t2.order_num = 2
+        t2.order_num = 20
         t3 = self.Task('t3')
-        t3.order_num = 3
+        t3.order_num = 30
         self.pl.add(t1)
         self.pl.add(t2)
         self.pl.add(t3)
@@ -77,16 +77,16 @@ class GetLowestHighestOrderNumTest(unittest.TestCase):
         order_num = self.ll.get_lowest_order_num()
 
         # then
-        self.assertEqual(1, order_num)
+        self.assertEqual(10, order_num)
 
     def test_many_tasks_highest_returns_highest(self):
         # given
         t1 = self.Task('t1')
-        t1.order_num = 1
+        t1.order_num = 10
         t2 = self.Task('t2')
-        t2.order_num = 2
+        t2.order_num = 20
         t3 = self.Task('t3')
-        t3.order_num = 3
+        t3.order_num = 30
         self.pl.add(t1)
         self.pl.add(t2)
         self.pl.add(t3)
@@ -95,4 +95,4 @@ class GetLowestHighestOrderNumTest(unittest.TestCase):
         order_num = self.ll.get_highest_order_num()
 
         # then
-        self.assertEqual(3, order_num)
+        self.assertEqual(30, order_num)
