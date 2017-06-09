b6ca435cb88b337c071b802a0a22a82c1ca60230
izrik <izrik@izrik.com>
2017-06-08 23:18:26 -0500

    Test for empty lists.

--- tests/ll_do_import_data_tests.py
+++ tests/ll_do_import_data_tests.py
@@ -36,3 +36,141 @@ class LogicLayerDoImportDataTest(unittest.TestCase):
         self.assertEqual(0, self.pl.attachment_query.count())
         self.assertEqual(0, self.pl.user_query.count())
         self.assertEqual(0, self.pl.option_query.count())
+
+    def test_do_import_data_empty_tasks(self):
+        # given
+        src = '{"tasks":[]}'
+
+        # precondition
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+        # when
+        self.ll.do_import_data(json.loads(src))
+
+        # then
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+    def test_do_import_data_empty_tags(self):
+        # given
+        src = '{"tags":[]}'
+
+        # precondition
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+        # when
+        self.ll.do_import_data(json.loads(src))
+
+        # then
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+    def test_do_import_data_empty_notes(self):
+        # given
+        src = '{"notes":[]}'
+
+        # precondition
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+        # when
+        self.ll.do_import_data(json.loads(src))
+
+        # then
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+    def test_do_import_data_empty_attachments(self):
+        # given
+        src = '{"attachments":[]}'
+
+        # precondition
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+        # when
+        self.ll.do_import_data(json.loads(src))
+
+        # then
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+    def test_do_import_data_empty_users(self):
+        # given
+        src = '{"users":[]}'
+
+        # precondition
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+        # when
+        self.ll.do_import_data(json.loads(src))
+
+        # then
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+    def test_do_import_data_empty_options(self):
+        # given
+        src = '{"options":[]}'
+
+        # precondition
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
+
+        # when
+        self.ll.do_import_data(json.loads(src))
+
+        # then
+        self.assertEqual(0, self.pl.task_query.count())
+        self.assertEqual(0, self.pl.tag_query.count())
+        self.assertEqual(0, self.pl.note_query.count())
+        self.assertEqual(0, self.pl.attachment_query.count())
+        self.assertEqual(0, self.pl.user_query.count())
+        self.assertEqual(0, self.pl.option_query.count())
