887536658394a0b506eba320b4a518d807611266
izrik <izrik@izrik.com>
2017-06-28 09:53:09 -0500

    Don't overwrite a valid id with None.

--- models/task.py
+++ models/task.py
@@ -48,7 +48,7 @@ class TaskBase(object):
         }
 
     def update_from_dict(self, d):
-        if 'id' in d:
+        if 'id' in d and d['id'] is not None:
             self.id = d['id']
         if 'summary' in d:
             self.summary = d['summary']
