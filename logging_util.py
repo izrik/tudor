8cec1e70ae895f53df9640a741641bcfa30fa2d2
izrik <izrik@izrik.com>
2017-07-11 23:38:41 -0500

    Use the default logging level.

--- logging_util.py
+++ logging_util.py
@@ -4,7 +4,7 @@ import logging
 config_was_set = False
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.WARN,
     format='%(relativeCreated)d:%(levelname)s:%(name)s:%(funcName)s:'
            # '%(pathname)s,%(lineno)d:'
            ' %(message)s')
