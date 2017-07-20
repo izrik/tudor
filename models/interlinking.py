00295ad24b9b7f404c62eae7a85b3ef1d80993df
izrik <izrik@izrik.com>
2017-07-20 09:06:31 -0500

    Extract a common base class from the interlinked collection classes.

--- models/interlinking.py
+++ models/interlinking.py
@@ -4,8 +4,8 @@ import logging_util
 from changeable import Changeable
 
 
-class OneToManySet(collections.MutableSet):
-    _logger = logging_util.get_logger_by_name(__name__, 'OneToManySet')
+class InterlinkedSet(collections.MutableSet):
+    _logger = logging_util.get_logger_by_name(__name__, 'InterlinkedSet')
 
     __change_field__ = None
     __attr_counterpart__ = None
@@ -34,6 +34,17 @@ class OneToManySet(collections.MutableSet):
     def __iter__(self):
         return self.set.__iter__()
 
+    def append(self, item):
+        self._logger.debug('{}: {}'.format(self.c.id2, item.id2))
+        self.add(item)
+
+    def __str__(self):
+        return str(self.set)
+
+
+class OneToManySet(InterlinkedSet):
+    _logger = logging_util.get_logger_by_name(__name__, 'OneToManySet')
+
     def add(self, item):
         self._logger.debug('{}: {}'.format(self.c.id2, item.id2))
         if item not in self.set:
@@ -42,10 +53,6 @@ class OneToManySet(collections.MutableSet):
             setattr(item, self.__attr_counterpart__, self.container)
             self.container._on_attr_changed(self.__change_field__,
                                             Changeable.OP_ADD, item)
-
-    def append(self, item):
-        self._logger.debug('{}: {}'.format(self.c.id2, item.id2))
-        self.add(item)
 
     def discard(self, item):
         self._logger.debug('{}: {}'.format(self.c.id2, item.id2))
@@ -56,35 +63,9 @@ class OneToManySet(collections.MutableSet):
             self.container._on_attr_changed(self.__change_field__,
                                             Changeable.OP_REMOVE, item)
 
-    def __str__(self):
-        return str(self.set)
 
-
-class ManyToManySet(collections.MutableSet):
+class ManyToManySet(InterlinkedSet):
     _logger = logging_util.get_logger_by_name(__name__, 'ManyToManySet')
-
-    __change_field__ = None
-    __attr_counterpart__ = None
-
-    def __init__(self, container):
-        if container is None:
-            raise ValueError('container cannot be None')
-
-        self.container = container
-        self.set = set()
-
-    @property
-    def c(self):
-        return self.container
-
-    def __len__(self):
-        return len(self.set)
-
-    def __contains__(self, item):
-        return self.set.__contains__(item)
-
-    def __iter__(self):
-        return self.set.__iter__()
 
     def add(self, item):
         self._logger.debug('{}: {}'.format(self.c.id2, item.id2))
@@ -94,10 +75,6 @@ class ManyToManySet(collections.MutableSet):
             getattr(item, self.__attr_counterpart__).add(self.container)
             self.container._on_attr_changed(self.__change_field__,
                                             Changeable.OP_ADD, item)
-
-    def append(self, item):
-        self._logger.debug('{}: {}'.format(self.c.id2, item.id2))
-        self.add(item)
 
     def discard(self, item):
         self._logger.debug('{}: {}'.format(self.c.id2, item.id2))
