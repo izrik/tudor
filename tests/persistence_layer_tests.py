74c5cc973d34e33a9ca5b8a907f0b7f691464dd4
izrik <izrik@izrik.com>
2017-07-24 14:29:55 -0500

    Test the as_iterable function.

--- tests/persistence_layer_tests.py
+++ tests/persistence_layer_tests.py
@@ -12,6 +12,7 @@ from models.tag import Tag
 from models.task import Task
 from models.user import User
 import logging_util
+from persistence_layer import as_iterable
 
 
 class PersistenceLayerTest(unittest.TestCase):
@@ -4035,3 +4036,13 @@ class PersistenceLayerDeletionTest(unittest.TestCase):
         self.assertIsNone(a1.task_id)
         self.assertIsNone(a2.task_id)
         self.assertIsNone(a3.task_id)
+
+
+class AsIterableTest(unittest.TestCase):
+    def test_iterable_returns_same(self):
+        # expect
+        self.assertEquals([1, 2, 3], as_iterable([1, 2, 3]))
+
+    def test_non_iterable_returns_tuple(self):
+        # expect
+        self.assertEqual((4,), as_iterable(4))
