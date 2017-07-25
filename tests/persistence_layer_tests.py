d48863c8ee60d3c0696f383e2fa9aa34dddb70ec
izrik <izrik@izrik.com>
2017-07-25 14:51:13 -0500

    Test creation of different domain object types from db objects.

--- tests/persistence_layer_tests.py
+++ tests/persistence_layer_tests.py
@@ -4450,3 +4450,96 @@ class CreateDomainFromDbTest(unittest.TestCase):
             Exception,
             self.pl._create_domain_object_from_db_object,
             dbtask)
+
+    def test_create_attachment_from_db(self):
+        # given
+        dbatt = self.pl.DbAttachment('att')
+        self.pl.db.session.add(dbatt)
+        self.pl.db.session.commit()
+        # precondition
+        self.assertIsNotNone(dbatt.id)
+        # when
+        result = self.pl._create_domain_object_from_db_object(dbatt)
+        # then
+        self.assertIsNotNone(result)
+
+        self.assertIsInstance(result, Attachment)
+        self.assertEqual(dbatt.timestamp, result.timestamp)
+        self.assertEqual(dbatt.path, result.path)
+        self.assertEqual(dbatt.filename, result.filename)
+        self.assertEqual(dbatt.description, result.description)
+
+    def test_create_note_from_db(self):
+        # given
+        dbnote = self.pl.DbNote('note')
+        self.pl.db.session.add(dbnote)
+        self.pl.db.session.commit()
+        # precondition
+        self.assertIsNotNone(dbnote.id)
+        # when
+        result = self.pl._create_domain_object_from_db_object(dbnote)
+        # then
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, Note)
+        self.assertEqual(dbnote.content, result.content)
+        self.assertEqual(dbnote.timestamp, result.timestamp)
+
+    def test_create_task_from_db(self):
+        # given
+        dbtask = self.pl.DbTask('task')
+        self.pl.db.session.add(dbtask)
+        self.pl.db.session.commit()
+        # precondition
+        self.assertIsNotNone(dbtask.id)
+        # when
+        result = self.pl._create_domain_object_from_db_object(dbtask)
+        # then
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, Task)
+        self.assertEqual(dbtask.summary, result.summary)
+        self.assertEqual(dbtask.description, result.description)
+
+    def test_create_tag_from_db(self):
+        # given
+        dbtag = self.pl.DbTag('tag')
+        self.pl.db.session.add(dbtag)
+        self.pl.db.session.commit()
+        # precondition
+        self.assertIsNotNone(dbtag.id)
+        # when
+        result = self.pl._create_domain_object_from_db_object(dbtag)
+        # then
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, Tag)
+        self.assertEqual(dbtag.value, result.value)
+        self.assertEqual(dbtag.description, result.description)
+
+    def test_create_user_from_db(self):
+        # given
+        dbuser = self.pl.DbUser('user')
+        self.pl.db.session.add(dbuser)
+        self.pl.db.session.commit()
+        # precondition
+        self.assertIsNotNone(dbuser.id)
+        # when
+        result = self.pl._create_domain_object_from_db_object(dbuser)
+        # then
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, User)
+        self.assertEqual(dbuser.email, result.email)
+        self.assertEqual(dbuser.hashed_password, result.hashed_password)
+
+    def test_create_option_from_db(self):
+        # given
+        dboption = self.pl.DbOption('key', 'value')
+        self.pl.db.session.add(dboption)
+        self.pl.db.session.commit()
+        # precondition
+        self.assertIsNotNone(dboption.id)
+        # when
+        result = self.pl._create_domain_object_from_db_object(dboption)
+        # then
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, Option)
+        self.assertEqual(dboption.key, result.key)
+        self.assertEqual(dboption.value, result.value)
