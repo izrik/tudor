9c4c5ced7738c1d40a170a60d895ab1de822622a
izrik <izrik@izrik.com>
2017-07-28 09:16:08 -0500

    Test the 'lazy' parameter to the DbTask constructor.

--- tests/persistence_layer_tests.py
+++ tests/persistence_layer_tests.py
@@ -5368,6 +5368,27 @@ class GetOptionTest(unittest.TestCase):
         self.assertIs(dboption, result)
 
 
+class DbTaskConstructorTest(unittest.TestCase):
+    def setUp(self):
+        self.pl = generate_pl()
+
+    def test_none_lazy_is_allowed(self):
+        # when
+        task = self.pl.DbTask('task', lazy={})
+        # then
+        self.assertIsInstance(task, self.pl.DbTask)
+
+    def test_empty_lazy_is_allowed(self):
+        # when
+        task = self.pl.DbTask('task', lazy={})
+        # then
+        self.assertIsInstance(task, self.pl.DbTask)
+
+    def test_non_empty_lazy_raises(self):
+        # expect
+        self.assertRaises(ValueError, self.pl.DbTask, 'task', lazy={'id': 1})
+
+
 class DbTaskFromDictTest(unittest.TestCase):
     def setUp(self):
         self.pl = generate_pl()
