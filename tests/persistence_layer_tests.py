ae07ee0e6184bb4bb9678a53894427d8a4dea541
izrik <izrik@izrik.com>
2017-07-29 14:03:20 -0500

    Test the DbTask.from_dict method.

--- tests/persistence_layer_tests.py
+++ tests/persistence_layer_tests.py
@@ -6611,3 +6611,84 @@ class DbTaskMakeChangeTest(unittest.TestCase):
         self.task.make_change(Task.FIELD_CHILDREN, Changeable.OP_REMOVE, child)
         # then
         self.assertEqual([], list(self.task.children))
+
+
+class DbTagFromDictTest(unittest.TestCase):
+    def setUp(self):
+        self.pl = generate_pl()
+        self.pl.create_all()
+
+    def test_empty_yields_empty_dbtag(self):
+        # when
+        result = self.pl.DbTag.from_dict({})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertIsNone(result.id)
+        self.assertIsNone(result.value)
+        self.assertIsNone(result.description)
+        self.assertEqual([], list(result.tasks))
+
+    def test_id_none_is_ignored(self):
+        # when
+        result = self.pl.DbTag.from_dict({'id': None})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertIsNone(result.id)
+
+    def test_valid_id_gets_set(self):
+        # when
+        result = self.pl.DbTag.from_dict({'id': 123})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertEqual(123, result.id)
+
+    def test_value_none_is_ignored(self):
+        # when
+        result = self.pl.DbTag.from_dict({'value': None})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertIsNone(result.value)
+
+    def test_valid_value_gets_set(self):
+        # when
+        result = self.pl.DbTag.from_dict({'value': 'abc'})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertEqual('abc', result.value)
+
+    def test_description_none_becomes_none(self):
+        # when
+        result = self.pl.DbTag.from_dict({'description': None})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertIsNone(result.description)
+
+    def test_valid_description_gets_set(self):
+        # when
+        result = self.pl.DbTag.from_dict({'description': 'abc'})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertEqual('abc', result.description)
+
+    def test_tasks_none_yields_empty(self):
+        # when
+        result = self.pl.DbTag.from_dict({'tasks': None})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertEqual([], list(result.tasks))
+
+    def test_tasks_empty_yields_empty(self):
+        # when
+        result = self.pl.DbTag.from_dict({'tasks': []})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertEqual([], list(result.tasks))
+
+    def test_tasks_non_empty_yields_same(self):
+        # given
+        task = self.pl.DbTask('task')
+        # when
+        result = self.pl.DbTag.from_dict({'tasks': [task]})
+        # then
+        self.assertIsInstance(result, self.pl.DbTag)
+        self.assertEqual([task], list(result.tasks))
