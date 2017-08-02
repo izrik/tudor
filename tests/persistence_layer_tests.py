5f6e20e3cb660d700ec7d19359c1f30e9b13761c
izrik <izrik@izrik.com>
2017-08-02 09:41:47 -0500

    Test the from_dict and make_change methods of DbOption.

--- tests/persistence_layer_tests.py
+++ tests/persistence_layer_tests.py
@@ -7700,3 +7700,125 @@ class DbUserMakeChangeTest(unittest.TestCase):
         self.user.make_change(User.FIELD_TASKS, Changeable.OP_REMOVE, task)
         # then
         self.assertEqual([], list(self.user.tasks))
+
+
+class DbOptionFromDictTest(unittest.TestCase):
+    def setUp(self):
+        self.pl = generate_pl()
+        self.pl.create_all()
+
+    def test_empty_yields_empty_dboption(self):
+        # when
+        result = self.pl.DbOption.from_dict({})
+        # then
+        self.assertIsInstance(result, self.pl.DbOption)
+        self.assertIsNone(result.key)
+        self.assertIsNone(result.value)
+
+    def test_key_none_is_ignored(self):
+        # when
+        result = self.pl.DbOption.from_dict({'key': None})
+        # then
+        self.assertIsInstance(result, self.pl.DbOption)
+        self.assertIsNone(result.key)
+
+    def test_valid_key_gets_set(self):
+        # when
+        result = self.pl.DbOption.from_dict({'key': 123})
+        # then
+        self.assertIsInstance(result, self.pl.DbOption)
+        self.assertEqual(123, result.key)
+
+    def test_value_none_is_ignored(self):
+        # when
+        result = self.pl.DbOption.from_dict({'value': None})
+        # then
+        self.assertIsInstance(result, self.pl.DbOption)
+        self.assertIsNone(result.value)
+
+    def test_valid_value_gets_set(self):
+        # when
+        result = self.pl.DbOption.from_dict({'value': 'something'})
+        # then
+        self.assertIsInstance(result, self.pl.DbOption)
+        self.assertEqual('something', result.value)
+
+
+class DbOptionMakeChangeTest(unittest.TestCase):
+    def setUp(self):
+        self.pl = generate_pl()
+        self.pl.create_all()
+        self.option = self.pl.DbOption('a', 'b')
+
+    def test_setting_key_sets_key(self):
+        # precondition
+        self.assertEqual('a', self.option.key)
+        # when
+        self.option.make_change(Option.FIELD_KEY, Changeable.OP_SET, 1)
+        # then
+        self.assertEqual(1, self.option.key)
+
+    def test_adding_key_raises(self):
+        # expect
+        self.assertRaises(
+            ValueError,
+            self.option.make_change,
+            Option.FIELD_KEY, Changeable.OP_ADD, 1)
+
+    def test_removing_key_raises(self):
+        # expect
+        self.assertRaises(
+            ValueError,
+            self.option.make_change,
+            Option.FIELD_KEY, Changeable.OP_REMOVE, 1)
+
+    def test_changing_key_raises(self):
+        # expect
+        self.assertRaises(
+            ValueError,
+            self.option.make_change,
+            Option.FIELD_KEY, Changeable.OP_CHANGING, 1)
+
+    def test_setting_value_sets_value(self):
+        # precondition
+        self.assertEqual('b', self.option.value)
+        # when
+        self.option.make_change(Option.FIELD_VALUE, Changeable.OP_SET,
+                                'something')
+        # then
+        self.assertEqual('something', self.option.value)
+
+    def test_adding_value_raises(self):
+        # expect
+        self.assertRaises(
+            ValueError,
+            self.option.make_change,
+            Option.FIELD_VALUE, Changeable.OP_ADD, 'something')
+
+    def test_removing_value_raises(self):
+        # expect
+        self.assertRaises(
+            ValueError,
+            self.option.make_change,
+            Option.FIELD_VALUE, Changeable.OP_REMOVE, 'something')
+
+    def test_changing_value_raises(self):
+        # expect
+        self.assertRaises(
+            ValueError,
+            self.option.make_change,
+            Option.FIELD_VALUE, Changeable.OP_CHANGING, 'something')
+
+    def test_non_option_field_raises(self):
+        # expect
+        self.assertRaises(
+            ValueError,
+            self.option.make_change,
+            Task.FIELD_SUMMARY, Changeable.OP_SET, 'value')
+
+    def test_invalid_field_raises(self):
+        # expect
+        self.assertRaises(
+            ValueError,
+            self.option.make_change,
+            'SOME_OTHER_FIELD', Changeable.OP_SET, 'value')
