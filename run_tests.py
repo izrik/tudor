fd63cf8b5d03d5b5dae1a3d6694317ca2f2be920
izrik <izrik@izrik.com>
2017-09-02 16:19:51 -0500

    Test the reorder_tasks method.

--- run_tests.py
+++ run_tests.py
@@ -53,6 +53,7 @@ from tests.logic_layer.create_new_note_tests import *
 from tests.logic_layer.get_edit_task_data_tests import *
 from tests.logic_layer.allowed_file_tests import *
 from tests.logic_layer.create_new_attachment_tests import *
+from tests.logic_layer.reorder_tasks_tests import *
 
 from tests.view_layer.route_tests import *
 
