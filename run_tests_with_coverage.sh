20421d2844fce52b797310e1a266c1ae1f3e7136
izrik <izrik@izrik.com>
2017-10-25 22:45:37 -0500

    Use the unittest module's discover mode, instead of hard-coding all of the tests to run.

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-coverage run --source=tudor,conversions,logic_layer,models,view_layer,persistence_layer,collections_util,logging_util --branch ./run_tests.py "$@" && \
+coverage run --source=tudor,conversions,logic_layer,models,view_layer,persistence_layer,collections_util,logging_util --branch -m unittest discover -s tests -p '*.py' -t . "$@" && \
 coverage html
