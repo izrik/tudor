453b2aed784665b4224cf9a6b22ab1d0a14fc3ce
izrik <izrik@izrik.com>
2017-10-25 22:49:35 -0500

    Tidy up the test script a little.

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-coverage run --source=tudor,conversions,logic_layer,models,view_layer,persistence_layer,collections_util,logging_util --branch -m unittest discover -s tests -p '*.py' -t . "$@" && \
-coverage html
+SOURCES=tudor,conversions,logic_layer,models,view_layer,persistence_layer,collections_util,logging_util
+coverage run --source=$SOURCES --branch -m unittest discover -s tests -p '*.py' -t . "$@" && \
+    coverage html
