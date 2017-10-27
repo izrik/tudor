669b2a0c1d91c857de8e8199ade4ebfada16b2b3
izrik <izrik@izrik.com>
2017-10-27 09:33:08 -0500

    Re-format the list of sources for readability.

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-SOURCES=tudor,conversions,logic_layer,models,view_layer,persistence_layer,collections_util,logging_util
+SOURCES=tudor,conversions,logic_layer,models,view_layer,persistence_layer,\
+collections_util,logging_util
+
 coverage run --source=$SOURCES --branch -m unittest discover -s tests -p '*.py' -t . "$@" && \
     coverage html
