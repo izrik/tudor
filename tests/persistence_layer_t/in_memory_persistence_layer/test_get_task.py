23987cc244a8d0a1ce4b96a8f1e96b821383eba8
izrik <izrik@izrik.com>
2017-10-27 15:55:21 -0500

    Copy the class directly. be explicit, rather than implicit.

--- tests/persistence_layer_t/in_memory_persistence_layer/test_get_task.py
+++ tests/persistence_layer_t/in_memory_persistence_layer/test_get_task.py
@@ -1,7 +1,35 @@
+from models.task import Task
 from tests.persistence_layer_t.in_memory_persistence_layer.\
     in_memory_test_base import InMemoryTestBase
-from tests.persistence_layer_t.test_get_task import GetTaskTest
 
 
-class InMemoryGetTaskTest(InMemoryTestBase, GetTaskTest):
-    pass
+# copied from ../test_get_task.py
+
+
+class GetTaskTest(InMemoryTestBase):
+    def setUp(self):
+        self.pl = self.generate_pl()
+        self.pl.create_all()
+
+    def test_none_returns_none(self):
+        # when
+        result = self.pl.get_task(None)
+        # then
+        self.assertIsNone(result)
+
+    def test_non_existent_task_returns_none(self):
+        # when
+        result = self.pl.get_task(2)
+        # then
+        self.assertIsNone(result)
+
+    def test_valid_task_returns_task(self):
+        # given
+        task = Task('task')
+        self.pl.add(task)
+        self.pl.commit()
+        # when
+        result = self.pl.get_task(1)
+        # then
+        self.assertIsNotNone(result)
+        self.assertIs(task, result)
