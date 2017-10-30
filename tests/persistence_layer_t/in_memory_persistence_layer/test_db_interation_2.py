7a14b7a7f0b24992ceacc1e6e704b1d5ce8f84f6
izrik <izrik@izrik.com>
2017-10-30 10:01:24 -0500

    Test DB interaction.

---
+++ tests/persistence_layer_t/in_memory_persistence_layer/test_db_interation_2.py
@@ -0,0 +1,32 @@
+from models.tag import Tag
+from tests.persistence_layer_t.in_memory_persistence_layer.\
+    in_memory_test_base import InMemoryTestBase
+
+
+class DatabaseInteractionTest(InMemoryTestBase):
+    def setUp(self):
+        self.pl = self.generate_pl()
+        self.pl.create_all()
+
+    def test_changes_after_get_are_also_tracked(self):
+        # given
+        tag1 = Tag('tag', description='a')
+        self.pl.add(tag1)
+        self.pl.commit()
+        tag = self.pl.get_tag_by_value('tag')
+        # precondition
+        self.assertEqual('a', tag.description)
+        # when
+        tag.description = 'b'
+        # then
+        self.assertEqual('b', tag.description)
+        # when
+        self.pl.commit()
+        # then
+        self.assertEqual('b', tag.description)
+        self.assertEqual('b', tag1.description)
+        # when
+        self.pl.rollback()
+        # then
+        self.assertEqual('b', tag.description)
+        self.assertEqual('b', tag1.description)
