a085ec4816f3043ada63c0120559ae63c479dc18
izrik <izrik@izrik.com>
2017-10-30 09:58:48 -0500

    Properly consider all object types.

--- in_memory_persistence_layer.py
+++ in_memory_persistence_layer.py
@@ -20,9 +20,22 @@ class InMemoryPersistenceLayer(object):
 
         self._tasks = []
         self._tasks_by_id = {}
+
         self._tags = []
         self._tags_by_id = {}
         self._tags_by_value = {}
+
+        self._users = []
+        self._users_by_id = {}
+
+        self._options = []
+        self._options_by_key = {}
+
+        self._notes = []
+        self._notes_by_id = {}
+
+        self._attachments = []
+        self._attachments_by_id = {}
 
     def create_all(self):
         pass
@@ -45,37 +58,100 @@ class InMemoryPersistenceLayer(object):
         for domobj in list(self._added_objects):
             tt = self._get_object_type(domobj)
             if tt == Attachment:
-                pass
+                if domobj.id is None:
+                    domobj.id = self._get_next_attachment_id()
+                else:
+                    if domobj.id in self._attachments_by_id:
+                        raise Exception(
+                            'There already exists a attachment with id '
+                            '{}'.format(domobj.id))
+                self._attachments.append(domobj)
+                self._attachments_by_id[domobj.id] = domobj
             elif tt == Note:
-                pass
+                if domobj.id is None:
+                    domobj.id = self._get_next_note_id()
+                else:
+                    if domobj.id in self._notes_by_id:
+                        raise Exception(
+                            'There already exists a note with id {}'.format(
+                                domobj.id))
+                self._notes.append(domobj)
+                self._notes_by_id[domobj.id] = domobj
             elif tt == Task:
                 if domobj.id is None:
                     domobj.id = self._get_next_task_id()
                 else:
                     if domobj.id in self._tasks_by_id:
                         raise Exception(
-                            'There already exists a task with id {}'.format(domobj.id))
+                            'There already exists a task with id {}'.format(
+                                domobj.id))
                 self._tasks.append(domobj)
                 self._tasks_by_id[domobj.id] = domobj
             elif tt == Tag:
+                if domobj.id is None:
+                    domobj.id = self._get_next_tag_id()
+                else:
+                    if domobj.id in self._tags_by_id:
+                        raise Exception(
+                            'There already exists a tag with id {}'.format(
+                                domobj.id))
                 self._tags.append(domobj)
                 self._tags_by_id[domobj.id] = domobj
                 self._tags_by_value[domobj.value] = domobj
-                pass
             elif tt == Option:
-                pass
+                # TODO: autogenerate key?
+                self._options.append(domobj)
+                self._options_by_key[domobj.id] = domobj
             elif tt == User:
-                pass
+                if domobj.id is None:
+                    domobj.id = self._get_next_user_id()
+                else:
+                    if domobj.id in self._users_by_id:
+                        raise Exception(
+                            'There already exists a user with id {}'.format(
+                                domobj.id))
+                self._users.append(domobj)
+                self._users_by_id[domobj.id] = domobj
             self._added_objects.remove(domobj)
         self._added_objects.clear()
 
         for domobj in self._tasks:
+            self._values_by_object[domobj] = domobj.to_dict()
+        for domobj in self._tags:
+            self._values_by_object[domobj] = domobj.to_dict()
+        for domobj in self._notes:
+            self._values_by_object[domobj] = domobj.to_dict()
+        for domobj in self._attachments:
+            self._values_by_object[domobj] = domobj.to_dict()
+        for domobj in self._users:
+            self._values_by_object[domobj] = domobj.to_dict()
+        for domobj in self._options:
             self._values_by_object[domobj] = domobj.to_dict()
 
     def _get_next_task_id(self):
         if not self._tasks_by_id:
             return 1
         return max(self._tasks_by_id.iterkeys()) + 1
+
+    def _get_next_tag_id(self):
+        if not self._tags_by_id:
+            return 1
+        return max(self._tags_by_id.iterkeys()) + 1
+
+    def _get_next_note_id(self):
+        if not self._notes_by_id:
+            return 1
+        return max(self._notes_by_id.iterkeys()) + 1
+
+    def _get_next_attachment_id(self):
+        if not self._attachments_by_id:
+            return 1
+        return max(self._attachments_by_id.iterkeys()) + 1
+
+    def _get_next_user_id(self):
+        if not self._users_by_id:
+            return 1
+        return max(self._users_by_id.iterkeys()) + 1
 
     def rollback(self):
         for t in self._added_objects:
