90d58b2b577bb3a77c84fc27c023b802f3b2aca1
izrik <izrik@izrik.com>
2017-11-16 13:54:27 -0600

    Use the in-memory PL by default for the LL tests.

--- tests/logic_layer_t/util.py
+++ tests/logic_layer_t/util.py
@@ -3,7 +3,7 @@ from logic_layer import LogicLayer
 from tudor import generate_app
 
 
-def generate_ll(db_uri='sqlite://', use_in_mem_pl=False):
+def generate_ll(db_uri='sqlite://', use_in_mem_pl=True):
     if use_in_mem_pl:
         pl = InMemoryPersistenceLayer()
         ll = LogicLayer('/tmp/tudor/uploads', 'txt,pdf,png,jpg,jpeg,gif', pl)
