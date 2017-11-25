8ba5d7e1264fd8ad5d841173696a1a21dfdae342
izrik <izrik@izrik.com>
2017-11-25 11:29:59 -0600

    Make upload_folder and allowed_extensions configurable.

--- tests/logic_layer_t/util.py
+++ tests/logic_layer_t/util.py
@@ -3,10 +3,15 @@ from logic_layer import LogicLayer
 from tudor import generate_app
 
 
-def generate_ll(db_uri='sqlite://', use_in_mem_pl=True):
+def generate_ll(db_uri='sqlite://', use_in_mem_pl=True, upload_folder=None,
+                allowed_extensions=None):
     if use_in_mem_pl:
         pl = InMemoryPersistenceLayer()
-        ll = LogicLayer('/tmp/tudor/uploads', 'txt,pdf,png,jpg,jpeg,gif', pl)
+        if upload_folder is None:
+            upload_folder = '/tmp/tudor/uploads'
+        if allowed_extensions is None:
+            allowed_extensions = 'txt,pdf,png,jpg,jpeg,gif'
+        ll = LogicLayer(upload_folder, allowed_extensions, pl)
         return ll
     else:
         app = generate_app(db_uri=db_uri)
