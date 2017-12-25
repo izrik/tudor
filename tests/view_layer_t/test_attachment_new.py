cedf2b2003d601882f955be4a61f77d38bbf392d
izrik <izrik@izrik.com>
2017-12-25 17:55:08 -0600

    Use mocks instead of generate_ll.

--- tests/view_layer_t/test_attachment_new.py
+++ tests/view_layer_t/test_attachment_new.py
@@ -2,9 +2,10 @@ import unittest
 from mock import Mock
 from werkzeug.exceptions import BadRequest
 
+from in_memory_persistence_layer import InMemoryPersistenceLayer
+from logic_layer import LogicLayer
 from models.task import Task
 from models.user import User
-from tests.logic_layer_t.util import generate_ll
 from tests.util import MockFileObject
 from tests.view_layer_t.util import generate_mock_request
 from view_layer import ViewLayer, DefaultRenderer
@@ -12,105 +13,99 @@ from view_layer import ViewLayer, DefaultRenderer
 
 class AttachmentNewTest(unittest.TestCase):
     def setUp(self):
-        self.ll = generate_ll()
-        self.pl = self.ll.pl
+        self.ll = Mock(spec=LogicLayer)
+        self.pl = Mock(spec=InMemoryPersistenceLayer)
         self.r = Mock(spec=DefaultRenderer)
         self.vl = ViewLayer(self.ll, None, renderer=self.r)
 
     def test_creates_new_attachment(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {'task_id': task.id}
-        files = {'filename': MockFileObject('/filename.txt')}
+        f = MockFileObject('/filename.txt')
+        files = {'filename': f}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
         # when
         result = self.vl.attachment_new(request, admin)
         # then
-        self.assertIsNotNone(result)
-        self.assertEqual(1, len(task.attachments))
-        att = list(task.attachments)[0]
-        self.assertEqual('', att.description)
-        self.assertIsNone(att.filename)
-        self.assertEqual('filename.txt', att.path)
-        self.r.redirect.assert_called()
+        self.ll.create_new_attachment.assert_called_once_with(
+            task.id, f, '', admin)
+        # and
+        self.r.url_for.assert_called_once_with('view_task', id=task.id)
+        self.r.redirect.assert_called_once_with(self.r.url_for.return_value)
+        self.assertIs(self.r.redirect.return_value, result)
 
     def test_description_none_yields_none(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {'task_id': task.id,
                 'description': None}
-        files = {'filename': MockFileObject('/filename.txt')}
+        f = MockFileObject('/filename.txt')
+        files = {'filename': f}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
         # when
-        self.vl.attachment_new(request, admin)
+        result = self.vl.attachment_new(request, admin)
         # then
-        self.assertEqual(1, len(task.attachments))
-        att = list(task.attachments)[0]
-        self.assertIsNone(att.description)
-        self.r.redirect.assert_called()
+        self.ll.create_new_attachment.assert_called_once_with(
+            task.id, f, None, admin)
+        # and
+        self.r.url_for.assert_called_once_with('view_task', id=task.id)
+        self.r.redirect.assert_called_once_with(self.r.url_for.return_value)
+        self.assertIs(self.r.redirect.return_value, result)
 
     def test_empty_description_yields_empty_description(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {'task_id': task.id,
                 'description': ''}
-        files = {'filename': MockFileObject('/filename.txt')}
+        f = MockFileObject('/filename.txt')
+        files = {'filename': f}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
         # when
-        self.vl.attachment_new(request, admin)
+        result = self.vl.attachment_new(request, admin)
         # then
-        self.assertEqual(1, len(task.attachments))
-        att = list(task.attachments)[0]
-        self.assertEqual('', att.description)
-        self.r.redirect.assert_called()
+        self.ll.create_new_attachment.assert_called_once_with(
+            task.id, f, '', admin)
+        # and
+        self.r.url_for.assert_called_once_with('view_task', id=task.id)
+        self.r.redirect.assert_called_once_with(self.r.url_for.return_value)
+        self.assertIs(self.r.redirect.return_value, result)
 
     def test_description_sets_description(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {'task_id': task.id,
                 'description': 'asdf'}
-        files = {'filename': MockFileObject('/filename.txt')}
+        f = MockFileObject('/filename.txt')
+        files = {'filename': f}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
         # when
-        self.vl.attachment_new(request, admin)
+        result = self.vl.attachment_new(request, admin)
         # then
-        self.assertEqual(1, len(task.attachments))
-        att = list(task.attachments)[0]
-        self.assertEqual('asdf', att.description)
-        self.r.redirect.assert_called()
+        self.ll.create_new_attachment.assert_called_once_with(
+            task.id, f, 'asdf', admin)
+        # and
+        self.r.url_for.assert_called_once_with('view_task', id=task.id)
+        self.r.redirect.assert_called_once_with(self.r.url_for.return_value)
+        self.assertIs(self.r.redirect.return_value, result)
 
     def test_null_file_object_raises(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {'task_id': task.id,
                 'description': 'asdf'}
         files = {'filename': None}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
         # expect
         self.assertRaises(
             BadRequest,
@@ -121,16 +116,13 @@ class AttachmentNewTest(unittest.TestCase):
 
     def test_null_filename_raises(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {'task_id': task.id,
                 'description': 'asdf'}
         files = {'filename': MockFileObject(None)}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
         # expect
         self.assertRaises(
             BadRequest,
@@ -141,16 +133,13 @@ class AttachmentNewTest(unittest.TestCase):
 
     def test_empty_filename_raises(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {'task_id': task.id,
                 'description': 'asdf'}
         files = {'filename': MockFileObject('')}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
         # expect
         self.assertRaises(
             BadRequest,
@@ -161,16 +150,14 @@ class AttachmentNewTest(unittest.TestCase):
 
     def test_extension_not_allowed_raises(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {'task_id': task.id,
                 'description': 'asdf'}
         files = {'filename': MockFileObject('/filename.exe')}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
+        self.ll.allowed_file.return_value = False
         # expect
         self.assertRaises(
             BadRequest,
@@ -181,15 +168,12 @@ class AttachmentNewTest(unittest.TestCase):
 
     def test_task_id_not_sepcified_raises(self):
         # given
-        task = Task('task')
-        self.pl.add(task)
-        self.pl.commit()
+        task = Mock(spec=Task)
+        task.id = 123
         form = {}
         files = {'filename': MockFileObject('/filename.exe')}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, len(task.attachments))
         # expect
         self.assertRaises(
             BadRequest,
--- tests/view_layer_t/test_import_.py
+++ tests/view_layer_t/test_import_.py
@@ -1,8 +1,9 @@
 import unittest
 from mock import Mock
 
+from in_memory_persistence_layer import InMemoryPersistenceLayer
+from logic_layer import LogicLayer
 from models.user import User
-from tests.logic_layer_t.util import generate_ll
 from tests.util import MockFileObject
 from tests.view_layer_t.util import generate_mock_request
 from view_layer import ViewLayer, DefaultRenderer
@@ -10,8 +11,8 @@ from view_layer import ViewLayer, DefaultRenderer
 
 class ImportTest(unittest.TestCase):
     def setUp(self):
-        self.ll = generate_ll()
-        self.pl = self.ll.pl
+        self.ll = Mock(spec=LogicLayer)
+        self.pl = Mock(spec=InMemoryPersistenceLayer)
         self.r = Mock(spec=DefaultRenderer)
         self.vl = ViewLayer(self.ll, None, renderer=self.r)
 
@@ -21,19 +22,24 @@ class ImportTest(unittest.TestCase):
             "id": 1,
             "summary":"summary"
         }]}'''
+        src_dict = {
+            "tasks": [{
+                "id": 1,
+                "summary": "summary",
+            }]
+        }
         form = {}
         files = {'file': MockFileObject(filename='', content=src)}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, self.pl.count_tasks())
         # when
         result = self.vl.import_(request, admin)
         # then
-        self.assertIsNotNone(result)
-        self.assertEqual(1, self.pl.count_tasks())
-        self.r.redirect.assert_called()
-        self.r.render_template.assert_not_called()
+        self.ll.do_import_data.assert_called_once_with(src_dict)
+        # and
+        self.r.url_for.assert_called_once_with('index')
+        self.r.redirect.assert_called_once_with(self.r.url_for.return_value)
+        self.assertIs(self.r.redirect.return_value, result)
 
     def test_imports_data_from_form(self):
         # given
@@ -41,32 +47,35 @@ class ImportTest(unittest.TestCase):
             "id": 1,
             "summary":"summary"
         }]}'''
+        src_dict = {
+            "tasks": [{
+                "id": 1,
+                "summary": "summary",
+            }]
+        }
         form = {'raw': src}
         files = {}
         request = generate_mock_request(form=form, files=files)
         admin = User('admin', is_admin=True)
-        # precondition
-        self.assertEqual(0, self.pl.count_tasks())
         # when
         result = self.vl.import_(request, admin)
         # then
-        self.assertIsNotNone(result)
-        self.assertEqual(1, self.pl.count_tasks())
-        self.r.redirect.assert_called()
-        self.r.render_template.assert_not_called()
+        self.ll.do_import_data.assert_called_once_with(src_dict)
+        # and
+        self.r.url_for.assert_called_once_with('index')
+        self.r.redirect.assert_called_once_with(self.r.url_for.return_value)
+        self.assertIs(self.r.redirect.return_value, result)
 
-    def test_get_request_returns_content(self):
+    def test_get_request_returns_template(self):
         # given
         request = generate_mock_request(method='GET')
         admin = User('admin', is_admin=True)
         # self.r.render_template
-        # precondition
-        self.assertEqual(0, self.pl.count_tasks())
         # when
         result = self.vl.import_(request, admin)
         # then
-        self.assertIsNotNone(result)
-        self.r.render_template.assert_called()
-        self.assertEqual(0, self.pl.count_tasks())
+        self.r.render_template.assert_called_once_with('import.t.html')
+        self.assertIs(self.r.render_template.return_value, result)
+        # and
+        self.r.url_for.assert_not_called()
         self.r.redirect.assert_not_called()
-        self.r.render_template.assert_called()
