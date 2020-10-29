fbc2b6f8676474320453b14fc14e4e93e4413f9a
izrik <izrik@izrik.com>
2020-10-29 12:35:57 -0500

    Correctly resolve the DB uri and secret key.

--- gtudor.py
+++ gtudor.py
@@ -1,6 +1,8 @@
-from tudor import generate_app, Config
+from tudor import generate_app, Config, get_db_uri, get_secret_key
 
 config = Config.from_environ()
+config.DB_URI = get_db_uri(config.DB_URI, config.DB_URI_FILE)
+config.SECRET_KEY = get_secret_key(config.SECRET_KEY, config.SECRET_KEY_FILE)
 config = Config.combine(config, Config.from_defaults())
 app = generate_app(db_uri=config.DB_URI, upload_folder=config.UPLOAD_FOLDER,
                    secret_key=config.SECRET_KEY,
