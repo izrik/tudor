13907c92e34015190cc53a92f2d37901da5b6f56
izrik <izrik@izrik.com>
2020-10-29 12:18:38 -0500

    Take defaults into account.

--- gtudor.py
+++ gtudor.py
@@ -1,6 +1,7 @@
 from tudor import generate_app, Config
 
 config = Config.from_environ()
+config = Config.combine(config, Config.from_defaults())
 app = generate_app(db_uri=config.DB_URI, upload_folder=config.UPLOAD_FOLDER,
                    secret_key=config.SECRET_KEY,
                    allowed_extensions=config.ALLOWED_EXTENSIONS)
