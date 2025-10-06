680e277aab9e403f1866c221a5893b6dda1c1391
=Opencode running grok-code-fast-1 <opencode@metaindu.com>
2025-10-06 18:23:55 -0500

    Switch to using postgres for tests.

--- tests/persistence_t/sqlalchemy/util.py
+++ tests/persistence_t/sqlalchemy/util.py
@@ -1,11 +1,20 @@
 import unittest
+import pytest
+from sqlalchemy import create_engine
 
 from tudor import generate_app
 
 
+def get_postgresql_url(postgresql):
+    info = postgresql.info
+    return f"postgresql://{info.user}:{info.password}@{info.host}:{info.port}/{info.dbname}"
+
+
 class PersistenceLayerTestBase(unittest.TestCase):
-    def setUp(self):
-        self.pl = self.generate_pl()
+    @pytest.fixture(autouse=True)
+    def _setup(self, postgresql):
+        url = get_postgresql_url(postgresql)
+        self.pl = self.generate_pl(db_uri=url)
         self.app_context = self.app.app_context()
         self.app_context.push()
         self.pl.create_all()
